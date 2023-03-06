fe28782da5922d52e55899f1fd52d3fea00b4006
unknown <you@example.com>
2023-03-06 23:04:40 +0000

    Manager questions added

--- index.js
+++ index.js
@@ -10,61 +10,130 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+// Will push team data to teamMembers Array
+const teamMembers = [];
 
-// User questions 
-const questions = [
-	{
-		type: "input",
-		name: "name",
-		message: "Please enter your name:",
-		validate: function (input) {
-			if (input === "") {
-				return "Your name cannot be blank!";
-			} else {
-				return true;
-			}
+// User starts with managers details
+const managerPrompt = () => {
+	return inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "name",
+				message: "Please enter your name:",
+				validate: function (input) {
+					if (input === "") {
+						return "Your name cannot be blank!";
+					} else {
+						return true;
+					}
+				},
+			},
+			{
+				type: "input",
+				name: "id",
+				message: "Please enter your ID:",
+				validate: function (input) {
+					if (isNaN(input)) {
+						return "Your ID must be a number!";
+					} else if (input.trim() === "") {
+						return "Your ID cannot be blank!";
+					} else {
+						return true;
+					}
+				},
+			},
+			{
+				type: "input",
+				name: "email",
+				message: "Please enter your email address:",
+				validate: function (input) {
+					if (input === "") {
+						return "Your email cannot be blank!";
+					} else {
+						return true;
+					}
+				},
+			},
+			{
+				type: "input",
+				name: "officeNumber",
+				message: "Please enter your office number:",
+				validate: function (input) {
+					if (isNaN(input)) {
+						return "Office number must be a number!";
+					} else if (input.trim() === "") {
+						return "Office number cannot be blank!";
+					} else {
+						return true;
+					}
+				},
+			},
+		])
+
+		.then((answers) => {
+			const manager = new Manager(
+				answers.name,
+				answers.id,
+				answers.email,
+				answers.officeNumber
+			);
+			teamMembers.push(manager);
+			menuPrompt();
+		});
+};
+
+const menuPrompt = () => {
+	return inquirer.prompt([
+		{
+			type: "input",
+			name: "name",
+			message: "What is the manager's name?",
 		},
-	},
-	{
-		type: "input",
-		name: "id",
-		message: "Please enter your ID:",
-		validate: function (input) {
-			if (input === isNaN) {
-				return "Your name cannot be blank!";
-			} else {
-				return true;
-			}
+		{
+			type: "list",
+			name: "department",
+			message: "Which department does the manager work in?",
+			choices: ["Sales", "Marketing", "Engineering", "Operations"],
 		},
-	},
-];
+		{
+			type: "number",
+			name: "yearsOfExperience",
+			message: "How many years of management experience does the manager have?",
+			validate: function (value) {
+				var valid = !isNaN(parseFloat(value));
+				return valid || "Please enter a number";
+			},
+		},
+	]);
+};
 
 // function to initialize program
-function init() {
-	console.log(
-		"\n",
-		"-------------------- Hi, Welcome Team Profile Generator --------------------",
-		"\n",
-		"\n",
-		"1) Answer the questions below to generate your Team Profile",
-		"\n",
-		"2) Please NOTE! Pressing enter will jump to the next question. To add new line use <br>.",
-		"\n",
-		"3) If you wish to exit, please press Ctrl + C.",
-		"\n"
-	);
-	inquirer.prompt(questions).then((answers) => {
-		writeToFile("./output/team.html", render({ ...answers }));
-		console.log(
-			"\n",
-			"------------- Your Team Profile has been Successfully Generated! -------------",
-			"\n",
-			"----- Below are your results:",
-			"\n",
-			answers
-		);
-	});
-}
+// function init() {
+// 	console.log(
+// 		"\n",
+// 		"-------------------- Hi, Welcome Team Profile Generator --------------------",
+// 		"\n",
+// 		"\n",
+// 		"1) Answer the questions below to generate your Team Profile",
+// 		"\n",
+// 		"2) Please NOTE! Pressing enter will jump to the next question. To add new line use <br>.",
+// 		"\n",
+// 		"3) If you wish to exit, please press Ctrl + C.",
+// 		"\n"
+// 	);
+// 	inquirer.prompt(questions).then((answers) => {
+// 		writeToFile("./output/team.html", render({ ...answers }));
+// 		console.log(
+// 			"\n",
+// 			"------------- Your Team Profile has been Successfully Generated! -------------",
+// 			"\n",
+// 			"----- Below are your results:",
+// 			"\n",
+// 			answers
+// 		);
+// 	});
+// }
 
 // function call to initialize program
 init();
